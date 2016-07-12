3fc850e9a01b529d26634759f05b35ec855ef861
Mark Veltzer <mark.veltzer@gmail.com>
2016-07-12 07:32:21 +0300

    example of viewParam

--- src/main/java/com/meta/beans/BasicView.java
+++ src/main/java/com/meta/beans/BasicView.java
@@ -2,19 +2,34 @@ package com.meta.beans;
 
 import java.io.Serializable;
 import java.util.List;
+import java.util.logging.Logger;
+
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
-import javax.faces.bean.ViewScoped;
+import javax.faces.bean.SessionScoped;
  
 @SuppressWarnings("serial")
 @ManagedBean(name="dtBasicView")
-@ViewScoped
+@SessionScoped
 public class BasicView implements Serializable {
+	
+	private final Logger log =
+	          Logger.getLogger(this.getClass().getName());
      
     private List<Car> cars;
-     
-    @ManagedProperty("#{carService}")
+    String carId;
+
+    public String getCarId() {
+		return carId;
+	}
+
+	public void setCarId(String carId) {
+		log.info("setting car id to "+carId);
+		this.carId = carId;
+	}
+
+	@ManagedProperty("#{carService}")
     private CarService service;
  
     @PostConstruct
@@ -28,5 +43,19 @@ public class BasicView implements Serializable {
  
     public void setService(CarService service) {
         this.service = service;
+    }
+    
+    public Car getCarById(String id) {
+    	log.info("searching for carid "+this+", "+id);
+    	for(Car c:cars) {
+    		if(c.getId().equals(id)) {
+    			return c;
+    		}
+    	}
+    	log.info("returning null");
+    	return null;
+    }
+    public Car getCurrentCar() {
+    	return getCarById(carId);
     }
 }
