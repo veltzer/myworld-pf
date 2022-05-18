c192eaabd582b652d6193104902f2f41786f27d1
Mark Veltzer <mark.veltzer@gmail.com>
2022-05-18 13:51:02 +0300

    new config

--- config/project.py
+++ config/project.py
@@ -1,4 +1,1 @@
-project_name = "myworld-pf"
-project_description = "Demos for the d language"
-project_long_description = project_description
-project_year_started = 2016
+description_short = "Myworld implemented in prime faces"
